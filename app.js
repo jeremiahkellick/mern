73badc6cc3099b3228b1680b5d1249347db41371
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-10-20 16:19:43 -0700

    Add users and events

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
+const events = require('./routes/api/events');
+const passport = require('passport');
+require('./config/passport')(passport);
 
 const app = express();
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
         .then(() => console.log('Connected to MongoDB successfully'))
         .catch(err => console.log(err));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
 app.get('/', (req, res) => res.send('Hello, world!'));
+app.use('/api/users', users);
+app.use('/api/events', events);
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/api/users.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
+const respondWithSignedUser = (res, user) => {
+  const payload = { id: user.id, name: user.name };
+  jwt.sign(
+    payload,
+    keys.secretOrKey,
+    { expiresIn: 3600 },
+    (err, token) => res.json({ success: true, token: `Bearer ${token}` })
+  );
+};
+
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) return res.status(422).json(errors);
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'A user with that email already exists';
+      return res.status(422).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+                  .then(user => respondWithSignedUser(res, user))
+                  .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) return res.status(422).json(errors);
+
+  const { email, password } = req.body;
+  
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      errors.email = 'No user with that email exists';
+      return res.status(404).json(errors);
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        respondWithSignedUser(res, user);
+      } else {
+        errors.password = 'Incorrect password';
+        return res.status(401).json(errors);
+      }
+    });
+  });
+});
+
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({ id: req.user.id, name: req.user.name, email: req.user.email });
+  }
+);
+
+module.exports = router;
